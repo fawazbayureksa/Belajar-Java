27f80bdc7e9e8c497477a82c85d5b8e59bb8a22d
fawazbayureksa <fawwazbayureksa@gmail.com>
2021-12-27 12:15:21 +0800

    Update operator pembanding

---
+++ OperatorPembanding.java
@@ -0,0 +1,25 @@
+public class OperatorPembanding
+{
+	public static void main(String [] args)
+	{
+		int nilaiA,nilaiB;
+		boolean hasil;
+
+
+		//input data variabel
+		nilaiA = 10;
+		nilaiB = 8;
+
+		// A lebih besar dari B
+
+		System.out.println("10 lebih besar dari 8 ?");
+		hasil = nilaiA > nilaiB;
+		System.out.println(hasil);
+
+		// A lebih besar dari B
+
+		System.out.println("10 lebih kecil dari 8 ?");
+		hasil = nilaiA < nilaiB;
+		System.out.println(hasil);
+	}
+}
