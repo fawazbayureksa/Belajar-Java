37bfe1ea1b87be8f23fc86fcfd9f7c8ab857c8cd
fawazbayureksa <fawwazbayureksa@gmail.com>
2021-12-27 11:58:15 +0800

    Udpate Keliling Persegi Panjang

---
+++ KelilingPersegiPanjang.java
@@ -0,0 +1,20 @@
+public class KelilingPersegiPanjang{
+
+	public static void main(String [] args)
+	{
+		// deklarasi varibale
+		double k,p,l;
+
+		// DATA VARIABEL
+
+		p = 10.0;
+		l = 6.0;
+
+		// Proses
+
+		k = (2*p)+(2*l);
+
+		System.out.println("Keliling Persegi Panjang : "+k);
+
+	}
+}
